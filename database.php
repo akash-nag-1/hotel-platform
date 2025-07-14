2bb4421923c9863f406bc8cef5ece423bf35d1af
akashnag <akashnag1800@gmail.com>
2025-07-14 09:46:51 +0530

    added

--- database.php
+++ database.php
@@ -1,5 +1,5 @@
 <?php
-$connect = mysqli_connect('db', 'root', 'your_mysql_root_password', 'mydatabase');
+$connect = mysqli_connect('db', 'root', 'myStrongPass123', 'mydatabase');
 
 if (mysqli_connect_errno($connect)) {
     echo 'Failed to connect: ' . mysqli_connect_error();
